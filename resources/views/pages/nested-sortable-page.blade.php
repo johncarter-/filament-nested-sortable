72b4761c78c3bd2613c441dc8ab870e9258c8462
John Carter <john@johncarter.co.uk>
2025-07-23 15:28:33 +0100

    Add note about having to use a component for the record item

--- resources/views/pages/nested-sortable-page.blade.php
+++ resources/views/pages/nested-sortable-page.blade.php
@@ -5,6 +5,7 @@
         @foreach ($records as $record)
             {{-- Must be @livewire else Alpine tries to bind to the item attribute  --}}
             {{-- Not working: <livewire:filament-nested-sortable::nested-item :item="$item" /> --}}
+            {{-- We must use a Livewire component here so we can use the Filament Action group else we have problems with the modals --}}
             @livewire('filament-nested-sortable::nested-record', ['record' => $record])
         @endforeach
     </div>
